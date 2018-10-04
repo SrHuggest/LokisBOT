cc006826811a1738cc1c233101c443dab0f68a34
SrHuggest <41216249+SrHuggest@users.noreply.github.com>
2018-10-04 17:41:46 -0400

    Add files via upload

--- comandos/autorole.js
+++ comandos/autorole.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async (client,message,args) => {
     if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("Vc não pode fazer isso.");
-  let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+  let autorole = JSON.parse(fs.readFileSync("./comandos/autorole.json", "utf8"));
   if (!args[0]) {
      autorole[message.guild.id] = {
          role: 0
      };
-    fs.writeFile("./autorole.json", JSON.stringfy(autorole), (err) => {
+    fs.writeFile("./comandos/autorole.json", JSON.stringfy(autorole), (err) => {
         if (err) console.log(err);
     });
   }
@@ -19,9 +19,12 @@ module.exports.run = async (bot, message, args) => {
     autorole[message.guild.id] = {
         role: role.id
     };
-    fs.writeFile("./autorole.json", JSON.stringify(autorole), (err) => {
+    fs.writeFile("./comandos/autorole.json", JSON.stringify(autorole), (err) => {
          if (err) console.log(err)
     });
 
     message.reply(`Autorole setado para o cargo <@&${role.id}>`);
   }
+module.exports.help = {
+    name: "autorole"
+}};
